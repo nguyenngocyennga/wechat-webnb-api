7fd2df4770b0edb8242de6e9fbfc509e286c1e67
Nga Nguyen <nganguyen.ny@gmail.com>
2019-08-07 00:42:48 +0800

    Add create & destroy methods for User to put/patch api easier

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < Api::V1::BaseController
   skip_before_action :verify_authenticity_token, only: [:update]
-  before_action :set_user, only: [:show, :update]
+  before_action :set_user, only: [:show, :update, :destroy]
   
   def index
     @users = User.all
@@ -9,12 +9,25 @@ class Api::V1::UsersController < Api::V1::BaseController
   def show
   end
   
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    else
+      render_error
+    end
+  end
+  
   def update
     if @user.update(user_params)
       render :show
     else
       render_error
     end
+  end
+  
+  def destroy
+    @user.destroy
+    render json: { message: "User was successfully deleted. "}
   end
   
   private
